a686fc03dfb1cd8baf16b2dfd6f29f714a39f542
Sepi <deet1022@gmail.com>
2016-11-29 18:31:32 +0900

    Update nuspec file (Add Dependency File)

--- SchoolMeal/SchoolMeal/Properties/AssemblyInfo.cs
+++ SchoolMeal/SchoolMeal/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("SchoolMeal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("provide the school meals by parsing Neis homepage.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SchoolMeal")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Sepi")]
+[assembly: AssemblyProduct("SchoolMealAPI")]
+[assembly: AssemblyCopyright("Copyright © Sepi 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로 
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
